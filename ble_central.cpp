c1ba38bd477538cb1f8f2d5b50cb321f699a72e4
kn1cht <16277200+kn1cht@users.noreply.github.com>
2019-03-18 01:12:48 +0900

    organize codes

--- ble_central.cpp
+++ ble_central.cpp
@@ -1,6 +1,10 @@
 #include <M5Stack.h>
 #include "ble_central.hpp"
 
+bool BleCentral::connected = false;
+
+bool BleCentral::doConnect = false;
+
 BleCentral::BleCentral() {
   Serial.println("Starting Arduino BLE Client application...");
   BLEDevice::init("");
@@ -8,71 +12,66 @@ BleCentral::BleCentral() {
   pBLEScan->setInterval(1349);
   pBLEScan->setWindow(449);
   pBLEScan->setActiveScan(true);
-  while(connected==false){
-    pBLEScan->start(5, false);
-    if (doConnect == true) {
-      if (connectToServer()) Serial.println("We are now connected to the BLE Server.");
-      else Serial.println("We have failed to connect to the server; there is nothin more we will do.");
-      doConnect = false;
-    }
-  }
+  openConnection();
 }
 
 void BleCentral::open() {
-  //open the rice cookers via BLE
-  while(connected==false){
-    pBLEScan->start(5, false);
-    if (doConnect == true) {
-      if (connectToServer())Serial.println("We are now connected to the BLE Server.");
-      else Serial.println("We have failed to connect to the server; We will restart scan.");
-      doConnect = false;
-    }
-  }
-  pRemoteCharacteristic->writeValue(01,true);
-  delay(700);
-  pRemoteCharacteristic->writeValue(03,true);
-  delay(100);
-  pRemoteCharacteristic->writeValue(02,true);
-  delay(700);
-  pRemoteCharacteristic->writeValue(03,true);
-  delay(100);
+  openConnection();
+  pRemoteCharacteristic->writeValue(01,true); delay(700);
+  pRemoteCharacteristic->writeValue(03,true); delay(100);
+  pRemoteCharacteristic->writeValue(02,true); delay(700);
+  pRemoteCharacteristic->writeValue(03,true); delay(100);
   pRemoteCharacteristic->writeValue(00,true);
 }
 
+void BleCentral::openConnection() {
+  while(connected == false) {
+    pBLEScan->start(5, false);
+    if(doConnect == true) {
+      Serial.print("Forming a connection to ");
+      Serial.println(myDevice->getAddress().toString().c_str());
+      pClient->setClientCallbacks(new MyClientCallback());
+      pClient->connect(myDevice);
+      Serial.println("Connected to server");
 
+      BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+      if(pRemoteService == nullptr) {
+        Serial.print("Failed to find our service UUID: ");
+        Serial.println(serviceUUID.toString().c_str());
+        pClient->disconnect();
+      }
+      Serial.println("Found our service");
 
-bool BleCentral::connectToServer() {
-    Serial.print("Forming a connection to ");
-    Serial.println(myDevice->getAddress().toString().c_str());
-    Serial.println(" - Created client");
-
-    pClient->setClientCallbacks(new MyClientCallback());
-
-    // Connect to the remove BLE Server.
-    pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
-    Serial.println(" - Connected to server");
-
-    // Obtain a reference to the service we are after in the remote BLE server.
-    BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
-    if (pRemoteService == nullptr) {
-      Serial.print("Failed to find our service UUID: ");
-      Serial.println(serviceUUID.toString().c_str());
-      pClient->disconnect();
-      return false;
+      pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+      if(pRemoteCharacteristic == nullptr) {
+        Serial.print("Failed to find our characteristic UUID: ");
+        Serial.println(charUUID.toString().c_str());
+        pClient->disconnect();
+      }
+      else {
+        Serial.println("We are now connected to the BLE Server.");
+        connected = true;
+      }
+      doConnect = false;
     }
-    Serial.println(" - Found our service");
+  }
+}
 
+void BleCentral::MyClientCallback::onDisconnect(BLEClient* pclient) {
+  connected = false;
+  Serial.println("Disconnected");
+}
 
-    // Obtain a reference to the characteristic in the service of the remote BLE server.
-    pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
-    if (pRemoteCharacteristic == nullptr) {
-      Serial.print("Failed to find our characteristic UUID: ");
-      Serial.println(charUUID.toString().c_str());
-      pClient->disconnect();
-      return false;
-    }
-    Serial.println(" - Found our characteristic");
-
-    connected = true;
-    return true;
+void BleCentral::MyAdvertisedDeviceCallbacks::onResult(BLEAdvertisedDevice advertisedDevice) {
+  Serial.print("BLE Advertised Device found: ");
+  Serial.println(advertisedDevice.toString().c_str());
+#ifdef DEBUG
+  if(advertisedDevice.getName() == "test") {
+#else
+  if(advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
+#endif
+    BLEDevice::getScan()->stop();
+    myDevice = new BLEAdvertisedDevice(advertisedDevice);
+    doConnect = true;
+  }
 }
--- ble_central.hpp
+++ ble_central.hpp
@@ -1,58 +1,34 @@
 #ifndef BLE_CENTRAL_HPP
 #define BLE_CENTRAL_HPP
 
+#define DEBUG
+
 #include <BLEDevice.h>
 
-//setting about BLE starts from here
-// The remote service we wish to connect to.
 static BLEUUID serviceUUID("12345678-9012-3456-7890-1234567890aa");
-// The characteristic of the remote service we are interested in.
 static BLEUUID    charUUID("12345678-9012-3456-7890-123456789022");
-static bool doConnect = false;
-static bool connected = false;
-static BLERemoteCharacteristic* pRemoteCharacteristic;
 static BLEAdvertisedDevice* myDevice;
-
-class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
- /**
-   * Called for each advertising BLE server.
-   */
-  void onResult(BLEAdvertisedDevice advertisedDevice) {
-    Serial.print("BLE Advertised Device found: ");
-    Serial.println(advertisedDevice.toString().c_str());
-
-#ifdef DEBUG
-    if(advertisedDevice.getName() == "test") {
-#else
-    if(advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
-#endif
-      BLEDevice::getScan()->stop();
-      myDevice = new BLEAdvertisedDevice(advertisedDevice);
-      doConnect = true;
-    } // Found our server
-  } // onResult
-}; // MyAdvertisedDeviceCallbacks
-//setting about bluetooth end
-
-class MyClientCallback : public BLEClientCallbacks {
-  void onConnect(BLEClient* pclient) {
-  }
-
-  void onDisconnect(BLEClient* pclient) {
-    connected = false;
-    Serial.println("onDisconnect");
-  }
-};
 
 class BleCentral {
   public:
     BleCentral();
     void open();
+    class MyClientCallback: public BLEClientCallbacks {
+      void onConnect(BLEClient* pclient) {}
+      void onDisconnect(BLEClient* pclient);
+    };
+    class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+        // Called for each advertising BLE server.
+        void onResult(BLEAdvertisedDevice advertisedDevice);
+    };
 
   private:
+    static bool connected;
+    static bool doConnect;
+    BLERemoteCharacteristic* pRemoteCharacteristic;
     BLEScan* pBLEScan = BLEDevice::getScan();
-    BLEClient*  pClient  = BLEDevice::createClient();
-    bool connectToServer();
+    BLEClient* pClient = BLEDevice::createClient();
+    void openConnection();
 };
 
 #endif // BLE_CENTRAL_HPP
